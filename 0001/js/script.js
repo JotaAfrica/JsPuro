35cbb44307e619c0adf54548a871e681372a1ba2
Jota Africa <jotaafrica@gmail.com>
2022-04-10 22:11:43 -0300

    commit

--- 0001/js/script.js
+++ 0001/js/script.js
@@ -2,9 +2,8 @@
  const Salta = "<br>"  ;
 
 function teste() {
-
       document.getElementById('info').innerHTML = 'linha do x'  + Salta ;
       document.getElementById('info').innerHTML = 'linha do y' + Salta ;  
-
-
   }
+
+  
